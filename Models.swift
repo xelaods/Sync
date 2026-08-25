6c445a6fca2b1182b010b5254875e47b0ed3bde9
hikazq <107014640+xelaods@users.noreply.github.com>
2026-08-26 03:18:52 +0900

    Create Models.swift

---
+++ Models.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+enum ShiftKind: String, Codable, CaseIterable, Identifiable {
+    case normal = "通常"
+    case holiday = "休日"
+    case night = "夜勤"
+
+    var id: String { rawValue }
+}
+
+struct Shift: Identifiable, Codable, Hashable {
+    var id = UUID()
+    var title: String = "勤務"
+    var start: Date = Date()
+    var end: Date = Date().addingTimeInterval(8 * 3600)
+    var breakMinutes: Int = 60
+    var kind: ShiftKind = .normal
+    var isExcluded: Bool = false
+    var memo: String = ""
+}
+
+struct WageSettings: Codable {
+    var baseWage: Int = 1200
+    var holidayWage: Int = 1500
+    var nightWage: Int = 1400
+}
